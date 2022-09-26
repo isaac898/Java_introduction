d19d9192c3f442286fa2dc9842a687090dde6e0e
isaac898 <112734454+isaac898@users.noreply.github.com>
2022-09-26 03:07:57 -0700

    finished CircleProperties

---
+++ src/chapter1/CircleProperties.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class CircleProperties {
+    public static void main(String[] args) {
+        // radius equals 5.5
+        //display area and perimeter
+        double RADIUS = 5.5 ;
+        double PI = 3.14;
+        double perimeter = 2 * RADIUS * PI;
+        double area = RADIUS * RADIUS * PI;
+        System.out.println(perimeter);
+        System.out.println(area);
+
+    }
+}
