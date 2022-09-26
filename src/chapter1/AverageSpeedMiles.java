c098e027ce47852973cea0e47b801689fbaf9e31
isaac898 <112734454+isaac898@users.noreply.github.com>
2022-09-26 03:17:33 -0700

    finished AverageSpeedMiles

---
+++ src/chapter1/AverageSpeedMiles.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class AverageSpeedMiles {
+    public static void main(String[] args) {
+        double MILES = 14 / 1.6 ;
+        double HOURS = 60 / 45.5 ;
+
+        double SPEED = MILES / HOURS;
+        System.out.println(SPEED);
+    }
+}
